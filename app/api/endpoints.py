3539d83df2c3294430b69cd228c23e1bb4d385ca
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-04-15 20:16:52 -0300

    docs: organiza rotas do Swagger por categorias utilizando Tags

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from app.services.ai_service import processar_texto_com_ia
 from app.models.schema import DadosTriagem
 
-router = APIRouter()
+router = APIRouter(tags=["Triagem"])
 
 @router.post("/triagem", response_model=DadosTriagem)
 def realizar_triagem(texto_bruto: str):
